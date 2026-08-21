ddd64370efbfba685701cc7efec7add448d9e92c
Japharidze <sergi.japharidze@gmail.com>
2026-08-21 07:42:31 +0400

    avoid default open of snacks explorer

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      explorer = {
+        replace_netrw = false, -- Prevents opening when launching nvim with a directory
+      },
+    },
+  },
+}
