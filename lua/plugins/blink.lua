0b5e9a8696d15d1831b00e17a5760b78f791be9e
Japharidze <sergi.japharidze@gmail.com>
2026-08-21 07:52:56 +0400

    remapping to kj

---
+++ lua/plugins/blink.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "saghen/blink.cmp",
+    opts = {
+      keymap = {
+        ["<C-k>"] = { "select_prev", "fallback_to_mappings" },
+        ["<C-j>"] = { "select_next", "fallback_to_mappings" }
+      }
+    }
+  }
+}
